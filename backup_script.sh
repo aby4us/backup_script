cbc01a761db41efae606d00b2b07497d5c2af58d
Abed Diab <aby4us@gmail.com>
2025-06-19 14:29:45 +0300

    מוודא שיש הרשאות לקריאה
    מקבל כקלט נתיב לתיקייה שצריך לגבות.
    מוודא שהתיקייה קיימת 
    מוודא שיש הרשאות לקריאה

--- backup_script.sh
+++ backup_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SOURCE_DIR="$1"
+BACKUP_DIR="$HOME/backups"
 
 if [ -z "$SOURCE_DIR" ]; then
   echo "שגיאה: נא לציין נתיב לתיקייה לגיבוי."
@@ -9,5 +10,10 @@ fi
 
 if [ ! -d "$SOURCE_DIR" ]; then
   echo "שגיאה: התיקייה '$SOURCE_DIR' לא קיימת."
+  exit 1
+fi
+
+if [ ! -r "$SOURCE_DIR" ]; then
+  echo "שגיאה: אין הרשאות קריאה לתיקייה '$SOURCE_DIR'."
   exit 1
 fi
