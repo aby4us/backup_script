a9f31066bc69e1a65408da8d34bad940481939e5
Abed Diab <aby4us@gmail.com>
2025-06-19 14:21:17 +0300

    Add files via upload
    מקבל כקלט נתיב לתיקייה שצריך לגבות.
    מוודא שהתיקייה קיימת

--- backup_script.sh
+++ backup_script.sh
@@ -2,8 +2,12 @@
 
 SOURCE_DIR="$1"
 
-
 if [ -z "$SOURCE_DIR" ]; then
   echo "שגיאה: נא לציין נתיב לתיקייה לגיבוי."
+  exit 1
+fi
+
+if [ ! -d "$SOURCE_DIR" ]; then
+  echo "שגיאה: התיקייה '$SOURCE_DIR' לא קיימת."
   exit 1
 fi
