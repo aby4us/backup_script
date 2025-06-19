0d57c5699ea3afd07291ecd568c44e83c1b430f4
Abed Diab <aby4us@gmail.com>
2025-06-19 14:00:56 +0300

    מקבל כקלט נתיב לתיקייה שצריך לגבות

--- backup_script.sh
+++ backup_script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 SOURCE_DIR="$1"
-BACKUP_DIR="$HOME/backups"
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-BACKUP_NAME="backup_$TIMESTAMP.tar.gz"
+
 
 if [ -z "$SOURCE_DIR" ]; then
   echo "שגיאה: נא לציין נתיב לתיקייה לגיבוי."
